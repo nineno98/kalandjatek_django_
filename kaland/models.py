b00751e6904a5488507e8d4f48a82b7efa6bca36
nineno98 <2023450@B001-15>
2024-04-29 17:55:08 +0200

    02

--- kaland/models.py
+++ kaland/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length = 255)
     description = models.TextField(blank = True, null = True)
-    targets = models.ManyToManyField(Location, on_delete= models.SET_NULL, blank = True, null = True)
+    targets = models.ManyToManyField('self', blank = True, null = True)
 
     def __str__(self):
         return self.name
